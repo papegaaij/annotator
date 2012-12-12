71222b96fcecbc53fac7dd5e4d5b05407cca2e49
Emond Papegaaij <emond.papegaaij@topicus.nl>
2012-12-12 21:13:42 +0100

    eerste paar java files

---
+++ src/main/java/nl/topicus/annotator/Annotator.java
@@ -0,0 +1,11 @@
+package nl.topicus.annotator;
+
+public class Annotator {
+
+	public Annotator() {
+	}
+
+	public <T> ClassAnnotator<T> annotate(Class<T> clazz) {
+		return null;
+	}
+}
---
+++ src/main/java/nl/topicus/annotator/ClassAnnotator.java
@@ -0,0 +1,5 @@
+package nl.topicus.annotator;
+
+public class ClassAnnotator<T> {
+	
+}
